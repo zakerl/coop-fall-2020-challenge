6d37507b0f55724b9419cc35831359bd297ffee6
Labeeb Hasan Zaker <labeebzaker@gmail.com>
2020-07-10 10:42:50 -0700

    made changes

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,3 +1,4 @@
+
 class EventSourcer {
   constructor() {
     this.value = 0;
@@ -10,6 +11,75 @@ class EventSourcer {
   bulk_undo(num) {}
   bulk_redo(num) {}
 }
+let initial_value = 0;
+let first_value =  initial_value;
+let numbers_array = [];
+let popped_array = [];
+let popped_number;
+
+function add(num){
+  initial_value += num;
+  numbers_array.push(initial_value);
+  return initial_value;
+}
+
+function subtract(num){
+  initial_value-=num;
+  numbers_array.push(initial_value)
+  return initial_value;
+}
+
+function undo(){
+  if(numbers_array.length === 0){
+    return first_value;
+  }
+  popped_number = numbers_array.pop();
+  popped_array.push(popped_number)
+  initial_value = numbers_array[numbers_array.length - 1];
+  return numbers_array[numbers_array.length - 1];
+}
+function redo(){
+  if(popped_array.length === 0){
+    let str = popped_number.toString() + "(nothing to redo)"
+    return str;
+  }
+  popped_number = popped_array.pop()
+  numbers_array.push(popped_number);
+  initial_value = numbers_array[numbers_array.length - 1];
+  return numbers_array[numbers_array.length - 1];
+}
+
+function bulk_undo(num){
+  if(numbers_array.length === 0){
+    return first_value;
+  }
+  for(let i = 0; i < num; i++){
+    if(numbers_array.length === 0){
+      return first_value;
+    }
+    popped_number = numbers_array.pop();
+    popped_array.push(popped_number)
+    initial_value = numbers_array[numbers_array.length - 1];
+  }
+  return numbers_array[numbers_array.length - 1];
+}
+function bulk_redo(num){
+  if(popped_array.length === 0){
+    let str = popped_number.toString() + "(nothing to redo)"
+    return str;
+  }
+  for(let i = 0; i < num;i++){
+    if(popped_array.length === 0){
+      let str = popped_number.toString() + "(nothing to redo)"
+      return str;
+    }
+    popped_number = popped_array.pop()
+    numbers_array.push(popped_number);
+    initial_value = numbers_array[numbers_array.length - 1];
+  }
+  return numbers_array[numbers_array.length - 1];
+}
+
 
 // ----- Do not modify anything below this line (needed for test suite) ------
 module.exports = EventSourcer;
